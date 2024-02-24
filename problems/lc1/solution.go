5f222fbb69f6d9491782c1eec2822c8d70191e5a
Oleg V <oleg.vasiliev@hotmail.com>
2024-02-24 13:23:46 +0300

    #1

---
+++ problems/lc1/solution.go
@@ -0,0 +1,22 @@
+package lc1
+
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return
+// indices of the two numbers such that they add up to target.
+//
+// You may assume that each input would have exactly one solution,
+// and you may not use the same element twice.
+//
+// You can return the answer in any order.
+
+func twoSum(nums []int, target int) []int {
+	var indexMap = make(map[int]int)
+	for i, n := range nums {
+		if mi, ok := indexMap[target-n]; ok {
+			return []int{i, mi}
+		}
+		indexMap[n] = i
+	}
+	return nil
+}
