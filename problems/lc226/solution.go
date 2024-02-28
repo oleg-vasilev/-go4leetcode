2623dcb09aa2d5860468c883195db7ee9dcf7c22
Oleg V <oleg.vasiliev@hotmail.com>
2024-02-28 19:49:01 +0300

    #226

---
+++ problems/lc226/solution.go
@@ -0,0 +1,19 @@
+package lc226
+
+// 226. Invert Binary Tree
+
+// Given the root of a binary tree, invert the tree, and return its root.
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
